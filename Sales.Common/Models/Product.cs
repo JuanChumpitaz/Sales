90c6dfbdf70d6e2f2ec3d5f34495da4ae7537def
JuanChumpitaz <46404463+JuanChumpitaz@users.noreply.github.com>
2019-01-07 17:52:30 -0500

    Cambios en Myper - x1

---
+++ Sales.Common/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales.Common.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public Decimal Price       {get;set;}
+        public bool isAvailable {get;set;}
+        public DateTime PublichOn { get; set; }
+    }
+}
